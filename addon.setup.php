98d07906c780e425a8d66d305c728ed087c9db86
Brian Litzinger <brian.litzinger@jamfsoftware.com>
2017-09-13 12:49:39 -0500

    Converted to EE3

---
+++ addon.setup.php
@@ -0,0 +1,12 @@
+<?php
+
+// Converted to EE3 by Brian Litzinger @ BoldMinded
+
+return array(
+    'name' => 'Thumber',
+    'version' => '2.0',
+    'author' => 'Rob Hodges and Andy Lulham (w/ Tim Kelty, Pete Eveleigh)',
+    'author_url' => 'http://www.electricputty.co.uk',
+    'description' => 'Create image thumbnails from PDF files',
+    'namespace' => 'ElectricPutty\Thumber',
+);
